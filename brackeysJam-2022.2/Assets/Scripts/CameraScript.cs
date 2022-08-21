666fe8122c5658fd5f73a8f4c7abb210b0c5b3be
Yaroslav <yarikmeisan@yandex.com>
2022-08-21 19:51:03 +0300

    Set up Camera Rotation

--- brackeysJam-2022.2/Assets/Scripts/CameraScript.cs
+++ brackeysJam-2022.2/Assets/Scripts/CameraScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // horizontal rotation speed
+    public float horizontalSpeed = 1f;
+    // vertical rotation speed
+    public float verticalSpeed = 1f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera cam;
+ 
     void Start()
     {
-        
+        cam = Camera.main;
     }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
+ 
+        yRotation += mouseX;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+ 
+        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
     }
 }
