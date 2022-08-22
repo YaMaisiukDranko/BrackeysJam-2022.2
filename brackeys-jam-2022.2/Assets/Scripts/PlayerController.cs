ed61ebcf95d6f29beff1cc05fd02c702ee4d4b08
Yaroslav <yarikmeisan@yandex.com>
2022-08-22 13:45:05 +0300

    Set up mouse follow by mouse

--- brackeys-jam-2022.2/Assets/Scripts/PlayerController.cs
+++ brackeys-jam-2022.2/Assets/Scripts/PlayerController.cs
@@ -5,5 +5,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    private Vector2 mousePos;
+    public Camera cam;
     
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    private void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
+    }
+
+    private void FixedUpdate()
+    {
+        //rotate player by mouse position
+        
+    }
 }
--- brackeys-jam-2022.2/Assets/Scripts/PlayerMovement.cs
+++ brackeys-jam-2022.2/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     private Vector2 movement;
+    public Vector2 mousePos;
+    public Camera cam;
 
     private void Start()
     {
@@ -18,10 +20,17 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        
+        
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
