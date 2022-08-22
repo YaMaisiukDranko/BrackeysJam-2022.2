c93156170816ab59055304dc0ca501f23050c2aa
Yaroslav <yarikmeisan@yandex.com>
2022-08-22 23:27:41 +0300

    Rename PlayerHealth.cs to HealthBar.cs

---
+++ brackeys-jam-2022.2/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthSlider;
+
+    public void SetMaxHealth(int health)
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+}
