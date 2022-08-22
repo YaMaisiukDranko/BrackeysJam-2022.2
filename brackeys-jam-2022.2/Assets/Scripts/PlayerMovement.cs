39df3d259ed2f6475fb7539d00e13ccd0c666487
Yaroslav <yarikmeisan@yandex.com>
2022-08-22 12:26:17 +0300

    Change Movement to old Input Manager

--- brackeys-jam-2022.2/Assets/Scripts/PlayerMovement.cs
+++ brackeys-jam-2022.2/Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+    }
 }
