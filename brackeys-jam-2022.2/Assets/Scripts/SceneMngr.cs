bd89f142d70348007095cc23a8dedd3897525895
Yaroslav <yarikmeisan@yandex.com>
2022-08-22 20:54:41 +0300

    Set up SceneManager, SceneMngr.cs

---
+++ brackeys-jam-2022.2/Assets/Scripts/SceneMngr.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMngr : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
