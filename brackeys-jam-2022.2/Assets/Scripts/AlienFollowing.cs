444d2d3f89cacb00f7af116e2465c071a134c29b
Yaroslav <yarikmeisan@yandex.com>
2022-08-22 19:16:56 +0300

    Set up Alien Following

--- brackeys-jam-2022.2/Assets/Scripts/AlienFollowing.cs
+++ brackeys-jam-2022.2/Assets/Scripts/AlienFollowing.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AlienFollowing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 3f;
+    public Transform target;
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (target != null)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            target = col.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            target = null;
+        }
     }
 }
