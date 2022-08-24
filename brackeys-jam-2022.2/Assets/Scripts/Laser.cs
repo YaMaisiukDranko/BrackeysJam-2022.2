0f6e169d045a864bd0037aa6179c8d1f198787f4
Yaroslav <yarikmeisan@yandex.com>
2022-08-24 18:12:11 +0300

    Create Laser.cs

---
+++ brackeys-jam-2022.2/Assets/Scripts/Laser.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public LayerMask layersToHit;
+
+    private void Update()
+    {
+        float angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+        Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 50f, layersToHit);
+        if (hit.collider == null)
+        {
+            transform.localScale = new Vector3(50f, transform.localScale.y, 1);
+            return;
+        }
+        transform.localScale = new Vector3(hit.distance, transform.localScale.y, 1);
+        Debug.Log(hit.collider.gameObject.name);
+    }
+}
