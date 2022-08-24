b797f9c0077aa04c962646ed454ff1828eecc09e
Yaroslav <yarikmeisan@yandex.com>
2022-08-24 15:09:59 +0300

    Add Headers to GunTypes

--- brackeys-jam-2022.2/Assets/Scripts/GunTypes.cs
+++ brackeys-jam-2022.2/Assets/Scripts/GunTypes.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
 public class GunTypes : ScriptableObject
 {
+    [Header("Gun Settings")]
     public new string name;
-    public float fireRate; //Bullets per second
     public Sprite GunSprite;
     public int damage;
     public int fireForce;
-    
+
+    [Header("Machine Gun")]
     public bool rapidFire;
+    public float fireRate; //Bullets per second
+    
+    [Header("Shotgun")]
     public bool shotgun;
+    public int amountOfBullets;
 }
