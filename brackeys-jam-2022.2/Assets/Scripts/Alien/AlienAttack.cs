76609081e97e29c6526e29847bfd6b5941243734
Yaroslav <yarikmeisan@yandex.com>
2022-08-25 17:36:37 +0300

    Set up Alien Attacks

--- brackeys-jam-2022.2/Assets/Scripts/Alien/AlienAttack.cs
+++ brackeys-jam-2022.2/Assets/Scripts/Alien/AlienAttack.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AlienAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int alienDamage;
+    public float attackSpeed;
+    public float canAttack;
+    
+
+    void Attack()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (attackSpeed <= canAttack)
+            {
+                other.gameObject.GetComponent<PlayerHealth>().TakeDamage(alienDamage);
+                canAttack = 0f;
+            }
+            else
+            {
+                canAttack += Time.deltaTime;
+            }
+        }
     }
 }
