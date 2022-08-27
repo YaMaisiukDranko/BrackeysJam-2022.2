3c39425c56bc63ad61aa2f2e51c5b72bd23cf5db
Yaroslav <yarikmeisan@yandex.com>
2022-08-27 12:55:16 +0300

    Create Animation Controller.cs

---
+++ brackeys-jam-2022.2/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    public Animator PlayerAnimator;
+    public enum MovementStates { WalkUp, WalkDown, WalkLeft, WalkRight, Idle }
+
+    public void UpdateAnimation()
+    {
+        
+    }
+}
