f09152d09b33030d6ff79c76b76b7173c6bc288d
Yaroslav <yarikmeisan@yandex.com>
2022-08-28 00:55:41 +0300

    Create GunRotate.cs

--- brackeys-jam-2022.2/Assets/Scripts/GunRotate.cs
+++ brackeys-jam-2022.2/Assets/Scripts/GunRotate.cs
@@ -3,33 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class GunRotate : MonoBehaviour
 {
-    public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    public Vector2 movement;
     public Vector2 mousePos;
     public Camera cam;
-
+    
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponentInParent<Rigidbody2D>();
         Cursor.visible = false;
     }
 
     private void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-        
-        
-       
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
     }
 }
