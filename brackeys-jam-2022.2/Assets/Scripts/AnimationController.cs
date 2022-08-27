116345ef5a9605f6eb3a3c1a538f5df60b4c2aae
Yaroslav <yarikmeisan@yandex.com>
2022-08-27 14:16:26 +0300

    Add hands

--- brackeys-jam-2022.2/Assets/Scripts/AnimationController.cs
+++ brackeys-jam-2022.2/Assets/Scripts/AnimationController.cs
@@ -7,40 +7,40 @@ public class AnimationController : MonoBehaviour
 {
     public Animator PlayerAnimator;
     public PlayerMovement playerMovement;
+    
     public enum MovementStates { WalkUp, WalkDown, WalkLeft, WalkRight, Idle }
-
+    MovementStates state;
     private void Start()
     {
-        
+        state = MovementStates.Idle;
     }
 
     public void UpdateAnimation()
     {
-        MovementStates state;
         // up/down
         if (playerMovement.movement.y > 0) //up
         {
-            
+            PlayerAnimator.SetTrigger("WalkUp");
         }
         else if (playerMovement.movement.y < 0) //down
         {
-            
+            PlayerAnimator.SetTrigger("WalkDown");
         }
         
         // right/left
         if (playerMovement.movement.x > 0) //right
         {
-            
+            PlayerAnimator.SetTrigger("WalkRight");
         }
         else if(playerMovement.movement.x < 0) //left
         {
-            
+            PlayerAnimator.SetTrigger("WalkLeft");
         }
         
         //idle
         if (playerMovement.movement.x == 0 && playerMovement.movement.y == 0)
         {
-            
+            PlayerAnimator.SetTrigger("Idle");
         }
     }
 }
--- brackeys-jam-2022.2/Assets/Scripts/Player/PlayerMovement.cs
+++ brackeys-jam-2022.2/Assets/Scripts/Player/PlayerMovement.cs
@@ -31,7 +31,7 @@ public class PlayerMovement : MonoBehaviour
         
         
         Vector2 lookDir = mousePos - rb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         rb.rotation = angle;
     }
 }
