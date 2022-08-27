bc613e2802869dde8dcf9ee658d5fc7f660ee8ba
ZacharyH777 <93843297+ZacharyH777@users.noreply.github.com>
2022-08-27 15:36:36 -0700

    ExtraTextures and things

---
+++ brackeys-jam-2022.2/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform PlayerTrans;
+    public Transform CameraTrans;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CameraTrans.position = new Vector3(PlayerTrans.position.x,PlayerTrans.position.y,CameraTrans.position.z);
+    }
+}
