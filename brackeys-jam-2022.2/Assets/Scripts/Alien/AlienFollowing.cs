06748b59b23a9c920fd7c4e553fc3e57df50b983
Sam <63943054+invadingdingo@users.noreply.github.com>
2022-08-28 04:25:31 -0500

    Add alien sprites/animation

--- brackeys-jam-2022.2/Assets/Scripts/Alien/AlienFollowing.cs
+++ brackeys-jam-2022.2/Assets/Scripts/Alien/AlienFollowing.cs
@@ -8,15 +8,27 @@ public class AlienFollowing : MonoBehaviour
     public float speed = 3f;
     public Transform target;
 
+    public Animator anim;
+    public bool attacking; 
+
+    public float prevX;
+
     private void Start()
     {
+        attacking = false;
         target = GameObject.FindWithTag("Player").transform;
     }
 
     private void Update()
-    {
+    {   
+        CheckDirection();
+        prevX = transform.position.x;
+        if (attacking) {
+            anim.Play("Attack");
+        }
         if (target != null)
         {
+            anim.Play("Run");
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.position, step);
         }
@@ -25,7 +37,8 @@ public class AlienFollowing : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.CompareTag("Player"))
-        {
+        {   
+            attacking = true;
             target = null;
         }
         
@@ -35,7 +48,16 @@ public class AlienFollowing : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            attacking = false;
             target = other.transform;
+        }
+    }
+
+    private void CheckDirection() {
+        if (transform.position.x > prevX) {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        } else if (transform.position.x < prevX) {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
