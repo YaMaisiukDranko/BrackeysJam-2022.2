fbce618502bbdd08e0ed81357267c816e5bb5bbb
Yaroslav <yarikmeisan@yandex.com>
2022-08-28 11:54:47 +0300

    Foot sounds

--- brackeys-jam-2022.2/Assets/Scripts/Player/PlayerMovement.cs
+++ brackeys-jam-2022.2/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,9 +10,11 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 movement;
     public Vector2 mousePos;
     public Camera cam;
+    private AudioSource _audioSource;
 
     private void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody2D>();
         Cursor.visible = false;
     }
@@ -22,14 +24,17 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition); 
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.D))
+        {
+            _audioSource.Play();
+        }
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-        
-        
-       
     }
 }
