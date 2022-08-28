12e688ab2581dbe64dbcb180d661b25d1b162ece
Yaroslav <yarikmeisan@yandex.com>
2022-08-28 09:43:51 +0300

    Set up enable particle system and game over panel

--- brackeys-jam-2022.2/Assets/Scripts/Player/PlayerController.cs
+++ brackeys-jam-2022.2/Assets/Scripts/Player/PlayerController.cs
@@ -5,5 +5,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public PlayerHealth PlayerHealth;
+    public GameObject particle;
+    public GameObject GameOverPanel;
+
+    private void Update()
+    {
+        PlayerHealth = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+        GameOver();
+    }
+
+    public void GameOver()
+    {
+        if (PlayerHealth.currentHealth <= 0)
+        {
+            particle.SetActive(true);
+            StartCoroutine(delay(1));
+        }
+    }
     
+    private IEnumerator delay(float duration)
+    {
+        GameOverPanel.SetActive(false);
+        yield return new WaitForSeconds(duration);
+        GameOverPanel.SetActive(true);
+    }
 }
