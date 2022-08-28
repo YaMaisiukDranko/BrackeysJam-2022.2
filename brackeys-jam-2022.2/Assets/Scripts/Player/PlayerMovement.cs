5233de8a510eb64295c128bb3a678e18252764b6
Yaroslav <yarikmeisan@yandex.com>
2022-08-28 12:26:08 +0300

    Sounds

--- brackeys-jam-2022.2/Assets/Scripts/Player/PlayerMovement.cs
+++ brackeys-jam-2022.2/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 movement;
     public Vector2 mousePos;
     public Camera cam;
-    private AudioSource _audioSource;
+    public AudioSource _audioSource;
 
     private void Start()
     {
@@ -25,12 +25,6 @@ public class PlayerMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
         
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.D))
-        {
-            _audioSource.Play();
-        }
     }
 
     private void FixedUpdate()
